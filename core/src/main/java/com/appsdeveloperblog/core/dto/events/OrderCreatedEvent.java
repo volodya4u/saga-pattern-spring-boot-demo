073161f92c5621456f33a4bebd7085b9e5c6968d
volodya4u <volodya4u@hotmail.com>
2026-01-07 23:25:43 +0200

    Publish OrderCreatedEvent

---
+++ core/src/main/java/com/appsdeveloperblog/core/dto/events/OrderCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.appsdeveloperblog.core.dto.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreatedEvent {
+
+    private UUID orderId;
+    private UUID customerId;
+    private UUID productId;
+    private Integer quantity;
+
+}
