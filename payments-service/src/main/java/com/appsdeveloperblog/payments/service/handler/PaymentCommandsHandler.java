ac0157ce4bbfddb32ea586f749dc762e76eb7158
volodya4u <volodya4u@hotmail.com>
2026-01-10 02:37:22 +0200

    Process paymentProcessedEvent and paymentFailedEvent

--- payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentCommandsHandler.java
+++ payments-service/src/main/java/com/appsdeveloperblog/payments/service/handler/PaymentCommandsHandler.java
@@ -2,11 +2,15 @@ package com.appsdeveloperblog.payments.service.handler;
 
 import com.appsdeveloperblog.core.dto.Payment;
 import com.appsdeveloperblog.core.dto.commands.ProcessPaymentCommand;
+import com.appsdeveloperblog.core.dto.events.PaymentFailedEvent;
+import com.appsdeveloperblog.core.dto.events.PaymentProcessedEvent;
 import com.appsdeveloperblog.core.exceptions.CreditCardProcessorUnavailableException;
 import com.appsdeveloperblog.payments.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -16,9 +20,15 @@ import org.springframework.stereotype.Component;
 public class PaymentCommandsHandler {
 
     private final PaymentService paymentService;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String paymentsEventsTopicName;
 
-    public PaymentCommandsHandler(PaymentService paymentService) {
+    public PaymentCommandsHandler(PaymentService paymentService,
+                                  KafkaTemplate<String, Object> kafkaTemplate,
+                                  @Value("${payments.events.topic.name}") String paymentsEventsTopicName) {
         this.paymentService = paymentService;
+        this.kafkaTemplate = kafkaTemplate;
+        this.paymentsEventsTopicName = paymentsEventsTopicName;
     }
 
     @KafkaHandler
@@ -32,8 +42,19 @@ public class PaymentCommandsHandler {
                     command.getProductQuantity()
             );
             Payment processedPayment = paymentService.process(payment);
+            PaymentProcessedEvent  paymentProcessedEvent = new PaymentProcessedEvent(
+                    processedPayment.getOrderId(),
+                    processedPayment.getId()
+            );
+            kafkaTemplate.send(paymentsEventsTopicName, paymentProcessedEvent);
         } catch (CreditCardProcessorUnavailableException e) {
             log.error(e.getLocalizedMessage(), e);
+            PaymentFailedEvent paymentFailedEvent = new PaymentFailedEvent(
+                    command.getOrderId(),
+                    command.getProductId(),
+                    command.getProductQuantity()
+            );
+            kafkaTemplate.send(paymentsEventsTopicName, paymentFailedEvent);
         }
     }
 
